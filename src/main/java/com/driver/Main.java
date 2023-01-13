85e8811a0058d3886833365fc515e3494f33463f
sowmya-pusapati <pusapatisowmya@gmail.com>
2023-01-13 12:53:59 +0530

    accessing the variable

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args){
+        RWOnly r=new RWOnly();
+        //r.name="sowmya" java: name has private access in com.driver.RWOnly
+        System.out.println("java: name has private access in com.driver.RWOnly");
+        r.setName("sowmya");
+        System.out.println(r.getName());
+    }
   
 }
